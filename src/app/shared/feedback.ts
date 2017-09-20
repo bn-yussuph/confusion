21569da2d4b549d9d5cbb6cd47454c0d7b3fc4bd
bn-yussuph <sulehajjaajj@gmail.com>
2017-09-20 09:37:15 +0000

    minor additions to feedback.ts

--- src/app/shared/feedback.ts
+++ src/app/shared/feedback.ts
@@ -2,10 +2,10 @@ export class Feedback {
 	firstname: string;
 	lastname: string;
 	telnum: number;
+	email: string;
 	agree: boolean;
 	contacttype: string;
 	message: string;
 };
 
-export const ContactType = 
-['None', 'Tel', 'Email'];
+export const ContactType = ['None', 'Tel', 'Email'];
